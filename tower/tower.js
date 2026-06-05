379af75f11932d08cea22205fbffb8f0990b6100
ZiggySaidGrub <grubsaidgrub@gmail.com>
2026-06-05 09:29:50 -0600

    square effect

--- tower/tower.js
+++ tower/tower.js
@@ -5,3 +5,15 @@ document.body.addEventListener("mousemove", (e) => {
     bgdiv.style.top = `${-e.pageY}px`;
 });
 
+let squarediv = document.getElementById("squarediv");
+
+for (let i = 0; i < squarediv.clientHeight; i += 64) {
+    for (let j = 0; j < squarediv.clientWidth; j += 64) {
+        let img = document.createElement("img");
+        img.src = `square${Math.floor(Math.random() * 3)}.png`;
+        img.width = "64";
+        img.height = "64"
+        img.classList.add("flex-item");
+        squarediv.appendChild(img);
+    }
+}
