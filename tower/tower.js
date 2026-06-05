3183b9d6cddb88fb4f5a73fa1f8e806e046bec45
ZiggySaidGrub <grubsaidgrub@gmail.com>
2026-06-05 11:32:34 -0600

    use href instead of replace

--- tower/tower.js
+++ tower/tower.js
@@ -59,7 +59,7 @@ async function fadeIn() {
     await new Promise((resolve) => setTimeout(resolve, fadeTime));
     if (parseFloat(fader.style.opacity) < 1) fadeIn();
     else if (won) {
-        window.location.replace("https://github.com/ZiggySaidGrub/sink");
+        window.location.href = "https://github.com/ZiggySaidGrub/sink";
     } else fadeOut();
 }
 
